65be5e935d674f3e381fd143e16a4836a35a7b97
andersonfrfilho <andersonfrfilho@gmail.com>
2020-05-17 02:35:09 -0300

    feature:finalli-appointments

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -2,12 +2,15 @@ import { Router } from 'express';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import AppointmentsController from '../controllers/AppointmentsController';
+import ProviderAppointmentsController from '../controllers/ProviderAppointmentsControllers';
 // SoC:Separation of Concerts (Separação de procupações)
 // DTO - Data transfer object
 const appointsmentsRouter = Router();
 const appointmentsController = new AppointmentsController();
+const providerAppointmentsController = new ProviderAppointmentsController();
 
 appointsmentsRouter.use(ensureAuthenticated);
 appointsmentsRouter.post('/', appointmentsController.create);
+appointsmentsRouter.get('/me', providerAppointmentsController.index);
 
 export default appointsmentsRouter;
